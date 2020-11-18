046d5d800c148c6f5d344d7586e3ac69109447bc
aarro029 <71415513+aarro029@users.noreply.github.com>
2020-11-17 20:43:17 -0800

    Update EZcompile_OpenSSL.py

--- EZcompile_OpenSSL.py
+++ EZcompile_OpenSSL.py
@@ -13,11 +13,11 @@ for file in os.listdir('.'):
 
 #This block of code prints a message, waits 5 secs, extracts OpenSSL_Tarball_Path, inserts extracted tarball contents into /OpenSSL, then prints message
 #Time.sleep intervals along with new lines are inserted throughout this script for the sake of aesthetics
-print('Extraction of OpenSSL Tarball has now commenced')
+print('Extraction Of OpenSSL Tarball Has Now Commenced...')
 time.sleep(5)
 subprocess.call(["tar", "-xvf", OpenSSL_Tarball_Path, "--strip-components=1", "-C", "OpenSSL"])
 print('\n')
-print('OpenSSL has been successfully extracted')
+print('OpenSSL Has Been Successfully Extracted')
 time.sleep(3)
 
 #Command that changes dir into the pre-made /OpenSSL, where the extracted tarball contents are now located
@@ -25,44 +25,43 @@ os.chdir('OpenSSL')
 
 #New lines seperate tar extraction step and initiates ./config step
 print('\n')
-print('\n')
-print('\nBeginning OpenSSL configuration step')
+print('\nBeginning OpenSSL Configuration...')
 time.sleep(3)
 os.system('./config')
 
 #Prints message and initiates make step
 print('\n')
 print('\n')
-print('\nCreating Makefile')
+print('\nCreating Makefile...')
 time.sleep(3)
 os.system('make')
 
 #Prints message and initiates make test step
 print('\n')
 print('\n')
-print('\nInitiating testing of Makefile')
+print('\nInitiating Testing Of Makefile...')
 time.sleep(3)
 os.system('make test')
 print('\n')
-print('\nMakefile looks good, proceeding with install...')
+print('\nMakefile Looks Good, Proceeding With Install...')
 
 #Prints message and initiates make install step
 time.sleep(3)
 print('\n')
 print('\n')
-print('\nNow Installing OpenSSL, Please Wait') 
+print('\nNow Installing OpenSSL, Please Wait...') 
 time.sleep(3)
 os.system('make install')
 
 #Updates lib conf files
 print('\n')
 print('\n')
-print('Updating and binding lib conf files, please wait...')
+print('Updating And Binding Lib Conf Files, Please Wait...')
 subprocess.call(["ldconfig", "/usr/lib", "/etc/ld.so.conf.d/libc.conf"])
 print('Binding completed')
 
 #Final successfull compilation message
 print('\n')
 print('\n')
-print('Congratulations, OpenSSL has been successfully compiled & installed')
+print('Congratulations, OpenSSL Has Been Successfully Compiled & Installed')
 time.sleep(3)
